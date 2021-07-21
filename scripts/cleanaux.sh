f773f9d7024ecf60c166f0d09722a65af6e589ea
Kenneth Hanson <khanson679@gmail.com>
2021-07-21 13:22:53 -0400

    Formatting. Make executable.

--- scripts/cleanaux.sh
+++ scripts/cleanaux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for dir in ${@}; do
-	for f in ${dir}/*.{aux,fdb_latexmk,fls,log,sta,thm}; do
+for dir in $@; do
+	for f in "${dir}/*.{aux,fdb_latexmk,fls,log,sta,thm}"; do
 		if [ -f $f ]; then rm $f; fi
 	done
 done

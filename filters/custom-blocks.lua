25c4388ea6bca8cb05b88e19f0c69fbcfc988096
Kenneth Hanson <khanson679@gmail.com>
2021-08-20 15:46:43 -0400

    Add exercise answer blocks.

--- filters/custom-blocks.lua
+++ filters/custom-blocks.lua
@@ -1,7 +1,6 @@
--- Filter to handle custom blocks
+-- Filter to handle custom div blocks
 
--- Convert custom div blocks to LaTeX environments
--- Convert only for LaTeX output. Pass through unchanged for HTML
+-- Convert custom div blocks to LaTeX environments.
 if FORMAT:match "latex" then
   function Div(elem)
     cls = elem.classes[1]
@@ -10,5 +9,23 @@ if FORMAT:match "latex" then
     table.insert(elem.content,
       pandoc.RawBlock("latex", "\\end{" .. cls .. "}"))
     return elem
+  end
+end
+
+-- Convert exercise answer blocks for HTML. Leave other classes unchanged.
+if FORMAT:match "html" then
+  function Div(elem)
+    cls = elem.classes[1]
+    if cls == "exranswer" then
+      table.insert(elem.content, 1,
+        pandoc.RawBlock("html", "<details class=\"exr-answer\">"))
+      table.insert(elem.content, 2,
+        pandoc.RawBlock("html", "<summary>Show Answer</summary>"))
+      table.insert(elem.content,
+        pandoc.RawBlock("html", "</details>"))
+      return elem.content
+    else
+      return elem
+    end
   end
 end
